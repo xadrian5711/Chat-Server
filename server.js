148408900aece229ea462f0a1c291b6c78154f27
xadrian5711 <xadrian5711@icloud.com>
2026-05-04 09:37:45 -0600

    creates logEvent function, it sends the log to the server.log file

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+const { time } = require("console");
+const fs = require("fs");
 const net = require("net");
+
+const logEvent = (message) => {
+  const timestamp = new Date().toISOString();
+  const logEntry = `[${timestamp}] ${message}\n`;
+
+  fs.appendFile("server.log", logEntry, (err) => {
+    if (err) console.error("failed to write to log file", err);
+  });
+};
 
 const server = net
   .createServer((client) => {
+    const clientId = `Client-${client.remotePort}`;
+
     client.write("Welcome to the server");
-    //Server code here
+
+    logEvent(`${clientId} connected`);
   })
   .listen(3000, () => {
     console.log("listening port 3000");
